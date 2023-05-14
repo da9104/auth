a316bc3523b1ba98eea66fa1fac4fb8038aabc3a
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-05-14 17:06:47 +0100

    login logout function.

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express")
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
+const ObjectID = require('mongodb').ObjectId
 const bodyParser = require("body-parser")
 const app = express()
 const dotenv = require('dotenv')
 const passport = require('passport')
-// const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const md5 = require('md5')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
+const flash = require('flash')
 dotenv.config()
+
+const usersCollection = require('./db').db().collection('users')
+let User = function(data, requestedPostId) {
+    this.data = data
+    this.errors = []
+    this.requestedPostId = requestedPostId
+}
+module.exports = User
 
 // let items = JSON.stringify(items);
 
 app.use(express.static("public"))
+app.use(express.json())
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
 let sessionOptions = session({
     secret: "This app has secrets",
     store: MongoStore.create({client: require('./db')}),
@@ -30,17 +40,53 @@ let sessionOptions = session({
 app.use(sessionOptions)
 app.use(passport.initialize())
 app.use(passport.session())
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3001/auth/google/secrets',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+},
 
-const usersCollection = require('./db').db().collection('users')
-let User = function(data) {
-    this.data = data
-    this.errors = []
-}
+function(accessToken, refreshToken, profile, cd) {
+    return new Promise(async (resolve, reject) => {
+     if (!this.errors.length) {
+         await User.findAndModify({
+             query: { _id: new ObjectID(this.requestedPostId)},
+             update: {
+               $setOnInsert: { googleId: profile._id }
+             },
+             new: true,   // return new doc if one is upserted
+             upsert: true // insert the document if it does not exist
+         })
+         resolve("success")
+     } else {
+            cd(err, user)
+            resolve("failure")
+      }
+    })
+    // User.findOrCreate({ googleId: profile.id}, function(err, user) {
+    //     return cd(err, user)
+    // })
+}))
+   
+
+app.use(flash())
+app.use(function(req, res, next) {
+    res.locals.errors = req.flash('errors')
+    if (req.session.user) {
+        req.session.user = req.session.user._id
+    }
+     else {
+        req.session.user = 0
+    }
+  // make user session data available from within view templates
+  res.locals.user = req.session.user
+  next()
+})
 
 User.prototype.cleanUp = function() {
     if (typeof(this.data.username) != "string") {this.data.username = ""}
     if (typeof(this.data.password) != "string") {this.data.password = ""}
-  
     // get rid of any bogus properties
     this.data = {
       username: this.data.username.trim().toLowerCase(),
@@ -63,7 +109,6 @@ User.prototype.validate = function() {
         let usernameExists = await usersCollection.findOne({username: this.data.username})
         if (usernameExists) {this.errors.push("That username is already taken.")}
       }
-    
       // Only if email is valid then check to see if it's already taken
       if (validator.isEmail(this.data.username)) {
         let emailExists = await usersCollection.findOne({username: this.data.username})
@@ -89,25 +134,54 @@ User.prototype.register = function() {
     })
 }
 
-module.exports = User
-
-
+User.prototype.login = function() {
+    return new Promise((resolve, reject) => {
+      this.cleanUp()
+      usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
+        if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+          this.data = attemptedUser
+          resolve("Congrats!")
+        } else {
+          reject("Invalid username / password.")
+        }
+      }).catch(function() {
+        reject("Please try again later.")
+      })
+    })
+}
 
 app.get('/', function(req, res) {
-    res.render('home')
+    if (req.session.user) {
+        res.render('secrets')
+    } else {
+        res.render('home')
+    }
+   // res.render('home')
 })
 
 app.get('/login', function(req, res) {
-    res.render('login')
+    res.render('login', {errors: req.flash('errors')} )
 })
 
 app.get('/register', function(req, res) {
     //  const items = await db.collection("users").find().toArray()
-      res.render('register')
+      res.render('register', {regiErrors: req.flash('regiErrors')})
   })
 
 app.post('/login', function(req, res) {
-
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    req.session.user = {username: user.data.username, _id: user.data._id}
+    req.session.save(function() {
+      res.render('secrets')
+    })
+  }).catch(function(e) {
+    console.log(e)
+    req.flash('errors', e)
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  })
 })
 
 app.post('/register', function(req, res) {
@@ -117,9 +191,22 @@ app.post('/register', function(req, res) {
      req.session.save(function() {
       return res.render('secrets')
       })
-    }).catch((err) => {
-      console.log(err)
-      return res.status(400).json(JSON.stringify(err));
+    }).catch((regiErrors) => {
+     regiErrors.forEach(function(error) {
+     req.flash('regiErrors', error)
+     })
+        req.session.save(function() {
+        console.log(regiErrors)
+        return res.redirect('/register')
+        })
+      // return res.redirect('/')
+      // return res.status(400).json(JSON.stringify(err));
+    })
+ })
+
+ app.get('/logout', function(req, res){
+    req.session.destroy(function() {
+       return res.redirect('/')
     })
  })
 
